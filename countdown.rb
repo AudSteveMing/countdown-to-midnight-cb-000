5f36177acc658e263c2696413dbba16c754c8213
Steve Cordova <cordovas@bc.edu>
2018-06-26 20:43:51 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,6 +9,10 @@ def countdown(number1)
 
 end
 
+def countdown_with_sleep(seconds_to_sleep)
+  sleep(seconds_to_sleep)
+end
+
 # def countdown(seconds_to_midnight)
 #   while seconds_to_midnight > 0
 #     puts "#{seconds_to_midnight} SECOND(S)!"
