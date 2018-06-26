064bcad3b477f4b4685d4161817bbeb738992cba
Steve Cordova <cordovas@bc.edu>
2018-06-26 20:40:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,18 @@
 #write your code here
 
-def countdown(number1)
-  While number > 0
-    puts "#{number1} SECOND(S)!"
-    number1 = number1 - 1
+# def countdown(number1)
+#   While number > 0
+#     puts "#{number1} SECOND(S)!"
+#     number1 = number1 - 1
+#   end
+#   "HAPPY NEW YEAR!"
+#
+# end
+
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
   end
   "HAPPY NEW YEAR!"
-
 end
